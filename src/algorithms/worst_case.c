1890eb6fe39a8b41e4b0ddc1b980b5f95b32e627
Kristo1702 <kghk25@student.aau.dk>
2025-11-21 12:28:47 +0100

    Initiated file
    Ready for work

---
+++ src/algorithms/worst_case.c
@@ -0,0 +1,9 @@
+#include "worst_case.h"
+
+
+
+int  worst_case(int layout[HEIGHT][WIDTH], node target) {
+    
+
+    return 1;
+}
---
+++ src/algorithms/worst_case.h
@@ -0,0 +1,9 @@
+#ifndef AUTOMATED_WAREHOUSE_WORST_CASE_H
+#define AUTOMATED_WAREHOUSE_WORST_CASE_H
+
+#include <stdio.h>
+#include "../utility/types.h"
+
+int  worst_case(int layout[HEIGHT][WIDTH], node target);
+
+#endif //AUTOMATED_WAREHOUSE_WORST_CASE_H
