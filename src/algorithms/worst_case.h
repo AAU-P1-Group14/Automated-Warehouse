809ffec9e8a115bf6396145004bb2683ecdd79d4
Kristo1702 <kghk25@student.aau.dk>
2025-11-24 10:22:55 +0100

    ready

---
+++ src/algorithms/worst_case.h
@@ -0,0 +1,22 @@
+
+#ifndef AUTOMATED_WORST_CASE_H
+#define AUTOMATED_WORST_CASE_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../utility/types.h"
+#include "../utility/misc.h"
+
+
+int worst_case(int layout[HEIGHT][WIDTH], node target, node position);
+void update_and_check(int layout[HEIGHT][WIDTH], node target, node position, int* found_target, int* found_dropoff);
+void check_if_target(int layout[HEIGHT][WIDTH], node target, node position, int* found_target);
+void check_if_dropoff(int layout[HEIGHT][WIDTH], node dropoff, node position, int* found_dropoff);
+
+
+#endif //AUTOMATED_WORST_CASE_H
+
+
+
+
