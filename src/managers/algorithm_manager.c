0432e3c66c3936e0aeb4ec9d6166fa58d98cb7fe
Flasser <soren.frylund.jakobsen@gmail.com>
2025-11-25 10:51:58 +0100

    Merged with main, added worst case.

--- src/managers/algorithm_manager.c
+++ src/managers/algorithm_manager.c
@@ -7,7 +7,7 @@ void run_algorithms(int layout[HEIGHT][WIDTH], node target_t, int bench, bool pr
     if (!procedural) printf("TARGET SHELF: (%d, %d)\n", target_t.y, target_t.x);
     else printf("TARGET SHELF: PROCEDURAL\n");
     printf("BENCHES: (%d)\n\n", bench);
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 3; i++) {
 
         // Input target in layout array
         layout[target_t.y][target_t.x] = target;
@@ -30,15 +30,12 @@ void run_algorithms(int layout[HEIGHT][WIDTH], node target_t, int bench, bool pr
             case 0:
                 clock_gettime(CLOCK_REALTIME, &timestamp1);
                 // Worst case algorithm (random movement)
-                for (int i = 0; i < bench-1; i++) {
-                    tiles_worst_case_total += worst_case(layout, target_t, (node){16, 31}, (node){16, 4}, (node){16, 4});
+                for (int i = 0; i < bench; i++) {
+                    total_tiles += worst_case(layout, target_t, (node){16, 31}, (node){16, 4}, (node){16, 4});
                 }
                 // One last run to store the path
                 force_clear_path(layout);
-                tiles_worst_case_total += worst_case(layout, target_t, (node){16, 31}, (node){16, 4}, (node){16, 4});
-
-                // Calculate the average tiles in each run
-                int tiles_worst_case_average = tiles_worst_case_total / bench;
+                total_tiles += worst_case(layout, target_t, (node){16, 31}, (node){16, 4}, (node){16, 4});
 
                 // Calculate the time it took
                 clock_gettime(CLOCK_REALTIME, &timestamp2);
@@ -50,7 +47,10 @@ void run_algorithms(int layout[HEIGHT][WIDTH], node target_t, int bench, bool pr
                 // Print out the result from worst case
                 printf("Worst case algorithm:\n");
                 print_array(layout, false);
-                if (tiles_worst_case_average > 0) printf("\nFinal route for worst case was average %d tiles\n\n", tiles_worst_case_average);
+                if (total_tiles/bench > 0) {
+                    printf("\nTotal tiles traveled for worst case was %d tiles\n", total_tiles);
+                    printf("Average route for worst case was %d tiles\n\n", total_tiles/bench);
+                }
                 else printf("\nWorst case never reached target or drop-off\n\n");
 
                 printf("Total benches for worst case took %lld micros\n", passtime);
@@ -95,10 +95,10 @@ void run_algorithms(int layout[HEIGHT][WIDTH], node target_t, int bench, bool pr
                 }
 
                 clock_gettime(CLOCK_REALTIME, &timestamp2);
-                long long current = timestamp1.tv_sec * 1000000LL + timestamp1.tv_nsec / 1000;
-                long long passed = timestamp2.tv_sec * 1000000LL + timestamp2.tv_nsec / 1000;
+                current = timestamp1.tv_sec * 1000000LL + timestamp1.tv_nsec / 1000;
+                passed = timestamp2.tv_sec * 1000000LL + timestamp2.tv_nsec / 1000;
 
-                long long passtime = passed - current;
+                passtime = passed - current;
 
                 // Print out the result from BFS
                 printf("BFS algorithm:\n");
--- src/managers/algorithm_manager.h
+++ src/managers/algorithm_manager.h
@@ -5,5 +5,6 @@
 #include "../utility/types.h"
 #include "../algorithms/bfs.h"
 #include "../algorithms/dfs.h"
+#include "../algorithms/worst_case.h"
 
 void run_algorithms(int layout[HEIGHT][WIDTH], node target_t, int bench, bool procedural, bool debug);
